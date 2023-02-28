751b2bd0b53577e6a33d131dda119cc9d5b8a5cc
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-02-28 18:00:28 +0200

    les9

--- react-router-budget/src/helpers.js
+++ react-router-budget/src/helpers.js
@@ -1,5 +1,29 @@
+export const waait = () =>
+  new Promise((res) => setTimeout(res, Math.random() * 2000));
+
+const generateRandomColor = () => {
+  const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+
+  return `${existingBudgetLength * 34} 65% 50%`;
+};
+
 export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
+};
+
+export const createBudget = ({ name, amount }) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    color: generateRandomColor(),
+  };
+  const existingBudgets = fetchData("budgets") ?? [];
+  return localStorage.setItem(
+    "budgets",
+    JSON.stringify([...existingBudgets, newItem])
+  );
 };
 
 export const deleteItem = ({ key }) => {
